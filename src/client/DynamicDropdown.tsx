6bc300dcb41be0b5a8fedb24b35bcfab041e60d1
alueddeke <a.lueddeke@hotmail.com>
2024-06-24 12:59:09 -0400

    added conditional link type

--- src/client/DynamicDropdown.tsx
+++ src/client/DynamicDropdown.tsx
@@ -1,26 +1,44 @@
 "use client";
-import React, { useState } from "react";
-import Link from "next/link";
+import React, { useState, ComponentType, forwardRef } from "react";
 
 interface DynamicDropdownProps {
   links: { href: string; label: string }[];
+  className?: string;
+  LinkComponent?: ComponentType<any>;
 }
 
-const DynamicDropdown: React.FC<DynamicDropdownProps> = ({ links }) => {
+const DynamicDropdown: React.FC<DynamicDropdownProps> = ({
+  links,
+  className,
+  LinkComponent,
+}) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
+  // Function to render a link, using Next.js Link if available
+  const renderLink = (href: string, label: string, index: number) => {
+    if (LinkComponent) {
+      return (
+        <li key={index}>
+          <LinkComponent href={href}>
+            <a>{label}</a>
+          </LinkComponent>
+        </li>
+      );
+    } else {
+      return (
+        <li key={index}>
+          <a href={href}>{label}</a>
+        </li>
+      );
+    }
+  };
+
   return (
-    <div>
+    <div className={className}>
       <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>Menu</button>
       {isDropdownOpen && (
         <ul>
-          {links.map((link, index) => (
-            <li key={index}>
-              <Link href={link.href}>
-                <a>{link.label}</a>
-              </Link>
-            </li>
-          ))}
+          {links.map((link, index) => renderLink(link.href, link.label, index))}
         </ul>
       )}
     </div>
